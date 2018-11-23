7bcbcfecb9bbeb2db96d630a636f7387495a4c7b
invin1 <marafat@bgsu.edu>
2018-11-23 04:31:31 -0500

    minor changes in simple factory pattern

--- src/main/java/design/patterns/simplefactorypattern/algorithms/SortAlgorithm.java
+++ src/main/java/design/patterns/simplefactorypattern/algorithms/SortAlgorithm.java
@@ -1,8 +1,8 @@
 package design.patterns.simplefactorypattern.algorithms;
 
 public interface SortAlgorithm {
-
     String sort();
+
     default void reverse() {
         System.out.println("reverse the collection first");
     }
